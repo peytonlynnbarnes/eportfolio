40269f767a0da29ee54653fcc1511f29bd2b94f0
peytonlynnbarnes <peyton0330@gmail.com>
2025-04-26 16:47:30 -0400

    merged

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/projects", (req, res) => {
     res.send("Server is ready");
 });
 
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000 ");
 });
+
 
 
