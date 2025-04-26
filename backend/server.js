82cb214942b7f42100a3b0cac3d3b00e32836ff2
peytonlynnbarnes <peyton0330@gmail.com>
2025-04-26 17:30:17 -0400

    dontenv config

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
 const app = express();
 
-app.get("/projects", (req, res) => {
-    res.send("Server is ready");
-});
+app.get("/projects", (req, res) => {});
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000 ");
 });
 
